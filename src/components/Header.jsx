7d432832e631806c479e1dfce9e133ff460d1e01
Pedro De Lorenzo <161749002+PedroLorenzop@users.noreply.github.com>
2024-09-19 20:52:38 -0300

    Filtros e childrens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import{NavLink} from "react-router-dom";
 export default function Header(){
     return(
         <>
-            <header>
+            <header className="bg-purple-800 text-white flex justify-around h-14 items-center">
                 <div>
                     <h1>Portal Filmes</h1>
                 </div>
                 <nav>
-                    <ul>
+                    <ul className="flex gap-4">
                         <li><NavLink to = "/">Home</NavLink></li>
                         <li><NavLink to = "/listaFilme">Lista de Filmes</NavLink></li>
                         <li><NavLink to = "/generos">Generos</NavLink></li>
